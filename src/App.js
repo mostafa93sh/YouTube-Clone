ceed7c960598fa669e663dba80b4cb47a0afbb74
mostafa93sh <mostafa.khalel.93@gmail.com>
2023-04-27 05:31:28 +0200

    adding Router to App.js and its components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import NavBar from "./components/NavBar";
+import Feed from "./components/Feed";
+import VideoDetails from "./components/VideoDetails";
+import ChannelDetails from "./components/ChannelDetails";
+import SearchFeed from "./components/SearchFeed";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Box>
+        <NavBar />
+        <Routes>
+          <Route path='/' exact element={<Feed />} />
+          <Route path='/videos/:id' element={<VideoDetails />} />
+          <Route path='/channel/:id' element={<ChannelDetails />} />
+          <Route path='/search/:searchterm' element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;
