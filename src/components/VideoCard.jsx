1ad934afe55bc789fb87f549cca1075b5c6e8724
mostafa93sh <mostafa.khalel.93@gmail.com>
2023-04-27 18:54:50 +0200

    fix security issue

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelTitle,
+  demoChannelUrl,
+  demoProfilePicture,
+} from "../utils/constants";
 
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Card>
+      <Link to={videoId ? `/videos/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{
+            width: 358,
+            height: 180,
+          }}
+        />
+      </Link>
+    </Card>
+  );
 };
 
 export default VideoCard;
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -9,7 +9,7 @@ const Videos = ({ videos }) => {
     <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
-          {item.id.videoId && <VideoCard />}
+          {item.id.videoId && <VideoCard video={item} />}
           {/* {item.id.channelId && <ChannelCard />} */}
         </Box>
       ))}
